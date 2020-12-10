4e42de8737cad86aed0c8f5385f171e7e2ec4e58
Jan Lopata <jan.lopata@rohlik.cz>
2020-12-10 22:35:54 +0100

    AOC 2020 - day 10 WIP

---
+++ src/main/java/cz/princu/adventofcode/year20/days/Day10.java
@@ -0,0 +1,158 @@
+package cz.princu.adventofcode.year20.days;
+
+import cz.princu.adventofcode.common.Day;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Day10 extends Day {
+
+    public static void main(String[] args) throws IOException {
+        new Day10().printParts();
+    }
+
+    @Override
+    public Object part1(String data) {
+
+        LinkedList<Long> adaptersSorted = getAdaptersSorted(data);
+
+        Map<Long, Integer> differencesCountMap = new HashMap<>();
+
+        for (int i = 0; i < adaptersSorted.size() - 1; i++) {
+
+            long diff = adaptersSorted.get(i + 1) - adaptersSorted.get(i);
+            differencesCountMap.putIfAbsent(diff, 0);
+
+            differencesCountMap.put(diff, differencesCountMap.get(diff) + 1);
+
+        }
+
+
+        return (long)
+                differencesCountMap.getOrDefault(1L, 0)
+                * differencesCountMap.getOrDefault(3L, 0);
+
+    }
+
+    private LinkedList<Long> getAdaptersSorted(String data) {
+        String[] lines = data.split("\n");
+
+        LinkedList<Long> adaptersSorted = new LinkedList<>();
+        adaptersSorted.addFirst(0L);
+
+        Arrays.stream(lines)
+                .mapToLong(Long::parseLong)
+                .sorted()
+                .forEach(adaptersSorted::add);
+
+        adaptersSorted.add(adaptersSorted.getLast() + 3);
+        return adaptersSorted;
+    }
+
+
+    @Override
+    public Object part2(String data) {
+
+        LinkedList<Long> adaptersSorted = getAdaptersSorted(data);
+
+        long[] dataArray = adaptersSorted.stream().mapToLong(it -> it).toArray();
+
+        return countValidWithRemoval(dataArray);
+    }
+
+    Long countValidWithRemoval(long[] dataArray) {
+
+        if (dataArray.length <= 4) {
+            return countValidRemovals(Arrays.copyOf(dataArray, dataArray.length));
+        }
+
+        int middle = dataArray.length / 2;
+
+
+        long[] left = Arrays.copyOfRange(dataArray, 0, middle + 1);
+        long[] right = Arrays.copyOfRange(dataArray, middle, dataArray.length);
+
+        long result = 1L;
+
+        result *= countValidWithRemoval(left);
+        result *= countValidWithRemoval(right);
+
+        if (canBeRemoved(dataArray, middle)) {
+
+            // removal
+            long removalResult = 1L;
+            left[left.length - 1] = dataArray[middle + 1];
+            right = Arrays.copyOfRange(dataArray, middle + 1, dataArray.length);
+            removalResult *= countValidWithRemoval(left);
+            removalResult *= countValidWithRemoval(right);
+
+            result += removalResult + 1;
+        }
+
+        return result;
+
+    }
+
+    private Long countValidRemovals(long[] smallData) {
+
+        if (smallData.length > 4)
+            throw new IllegalArgumentException("too big data");
+
+        if (smallData.length < 3)
+            return 1L;
+
+        if (smallData.length == 3) {
+            if (canBeRemoved(smallData, 1)) {
+                return 2L;
+            } else
+                return 1L;
+        }
+
+        // length 4
+        if (smallData[smallData.length - 1] - smallData[0] <= 3 )
+            return 4L;
+
+        long result = 1;
+        if (canBeRemoved(smallData, 1))
+            result++;
+
+        if (canBeRemoved(smallData, 2))
+            result++;
+
+        return result;
+
+    }
+
+
+    private boolean canBeRemoved(long[] data, int indexToRemove) {
+
+        return data[indexToRemove + 1] - data[indexToRemove - 1] <= 3;
+
+    }
+
+
+    @Override
+    public int getDayNumber() {
+        return 10;
+    }
+
+    @Override
+    public int getYear() {
+        return 2020;
+    }
+
+    @Builder
+    @Getter
+    @ToString
+    static class BagRule {
+        private final String outerBag;
+        private final Map<String, Integer> innerBagsMap;
+    }
+
+}
---
+++ src/test/java/cz/princu/adventofcode/year20/days/Day10Test.java
@@ -0,0 +1,140 @@
+package cz.princu.adventofcode.year20.days;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Day10Test {
+
+    Day10 tested = new Day10();
+
+    @Test
+    void testPart1_one_simpleData() {
+
+        String testData =
+                "16\n" +
+                        "10\n" +
+                        "15\n" +
+                        "5\n" +
+                        "1\n" +
+                        "11\n" +
+                        "7\n" +
+                        "19\n" +
+                        "6\n" +
+                        "12\n" +
+                        "4";
+
+        Assertions.assertEquals(35L, tested.part1(testData));
+
+    }
+
+    @Test
+    void testPart1_second_simpleData() {
+
+        String testData =
+                "28\n" +
+                        "33\n" +
+                        "18\n" +
+                        "42\n" +
+                        "31\n" +
+                        "14\n" +
+                        "46\n" +
+                        "20\n" +
+                        "48\n" +
+                        "47\n" +
+                        "24\n" +
+                        "23\n" +
+                        "49\n" +
+                        "45\n" +
+                        "19\n" +
+                        "38\n" +
+                        "39\n" +
+                        "11\n" +
+                        "1\n" +
+                        "32\n" +
+                        "25\n" +
+                        "35\n" +
+                        "8\n" +
+                        "17\n" +
+                        "7\n" +
+                        "9\n" +
+                        "4\n" +
+                        "2\n" +
+                        "34\n" +
+                        "10\n" +
+                        "3";
+
+        Assertions.assertEquals(220L, tested.part1(testData));
+
+    }
+
+    @Test
+    void testPart2_one_simpleData() {
+
+        String testData =
+                "16\n" +
+                        "10\n" +
+                        "15\n" +
+                        "5\n" +
+                        "1\n" +
+                        "11\n" +
+                        "7\n" +
+                        "19\n" +
+                        "6\n" +
+                        "12\n" +
+                        "4";
+
+        Assertions.assertEquals(8L, tested.part2(testData));
+
+    }
+
+    @Test
+    void testPart2_second_simpleData() {
+
+        String testData =
+                "28\n" +
+                        "33\n" +
+                        "18\n" +
+                        "42\n" +
+                        "31\n" +
+                        "14\n" +
+                        "46\n" +
+                        "20\n" +
+                        "48\n" +
+                        "47\n" +
+                        "24\n" +
+                        "23\n" +
+                        "49\n" +
+                        "45\n" +
+                        "19\n" +
+                        "38\n" +
+                        "39\n" +
+                        "11\n" +
+                        "1\n" +
+                        "32\n" +
+                        "25\n" +
+                        "35\n" +
+                        "8\n" +
+                        "17\n" +
+                        "7\n" +
+                        "9\n" +
+                        "4\n" +
+                        "2\n" +
+                        "34\n" +
+                        "10\n" +
+                        "3";
+
+        Assertions.assertEquals(19208L, tested.part2(testData));
+
+    }
+
+
+    @Test
+    void test_knownSubvariants() {
+
+        Assertions.assertEquals(7, tested.countValidWithRemoval(new long[]{0, 1, 2, 3, 4}));
+
+    }
+
+
+
+}
