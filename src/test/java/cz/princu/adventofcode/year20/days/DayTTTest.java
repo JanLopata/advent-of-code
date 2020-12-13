90903781ba0c917db61607007262c803faa4aa1b
Jan Lopata <jan.lopata@rohlik.cz>
2020-12-13 22:42:29 +0100

    AOC 2020 - add day template

---
+++ src/test/java/cz/princu/adventofcode/year20/days/DayTTTest.java
@@ -0,0 +1,28 @@
+package cz.princu.adventofcode.year20.days;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DayTTTest {
+
+    Day13 tested = new Day13();
+
+    @Test
+    void testPart1_simpleData() {
+
+        String testData = "";
+
+        Assertions.assertEquals(0L, tested.part1(testData));
+
+    }
+
+    @Test
+    void testPart2_one_simpleData() {
+
+        String testData = "";
+
+        Assertions.assertEquals(0L, tested.part2(testData));
+
+    }
+
+}
