01cadc997b63b012d6ab98f5b4cfab906ede14fe
Jan Lopata <jan.lopata@rohlik.cz>
2020-12-26 07:29:09 +0100

    AOC 2020 - Day 23 part I.

---
+++ src/main/java/cz/princu/adventofcode/year20/days/Day23.java
@@ -0,0 +1,122 @@
+package cz.princu.adventofcode.year20.days;
+
+import cz.princu.adventofcode.common.Day;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class Day23 extends Day {
+    public static void main(String[] args) throws IOException {
+        new Day23().printParts();
+    }
+
+    @Setter
+    private int rounds = 100;
+
+    @Override
+    public Object part1(String data) {
+
+        LinkedList<Integer> cups = new LinkedList<>();
+
+        for (char c : data.toCharArray()) {
+            cups.add(Integer.parseInt(String.valueOf(c)));
+        }
+
+        for (int i = 0; i < rounds; i++) {
+
+            performMove(cups);
+
+        }
+
+        rotate(cups, 1);
+
+        log.info("result: {}", cups.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+
+
+        return cups.stream().skip(1).map(String::valueOf).collect(Collectors.joining());
+    }
+
+    private void rotate(LinkedList<Integer> cups, int current) {
+
+        while (cups.getFirst() != current) {
+            Integer first = cups.pollFirst();
+            cups.add(first);
+        }
+
+    }
+
+    // current value is always first
+    private void performMove(LinkedList<Integer> cups) {
+
+        log.info("{}", cups.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+
+        Integer first = cups.pollFirst();
+
+        LinkedList<Integer> three = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            three.add(cups.pollFirst());
+        }
+
+        int destination = chooseDestinationCup(cups, first - 1);
+        cups.addLast(first);
+
+        rotate(cups, destination);
+        cups.removeFirst();
+
+        for (int i = 0; i < 3; i++) {
+            cups.addFirst(three.pollLast());
+        }
+
+        cups.addFirst(destination);
+
+        rotate(cups, first);
+        rotate(cups, cups.get(1));
+
+        log.info("{} destination {}",
+                cups.stream().map(String::valueOf).collect(Collectors.joining(" ")),
+                destination);
+
+    }
+
+    private int chooseDestinationCup(LinkedList<Integer> cups, Integer initDestination) {
+
+        final Set<Integer> existing = cups.stream().collect(Collectors.toSet());
+
+        int destination = initDestination;
+        while (true) {
+
+            if (existing.contains(destination))
+                return destination;
+
+            if (destination <= 1)
+                destination = 10;
+
+            destination--;
+        }
+
+    }
+
+    @Override
+    public Object part2(String data) {
+
+        String[] input = data.split("\n");
+
+        return 0L;
+    }
+
+
+    @Override
+    public int getDayNumber() {
+        return 23;
+    }
+
+    @Override
+    public int getYear() {
+        return 2020;
+    }
+}
---
+++ src/test/java/cz/princu/adventofcode/year20/days/Day23Test.java
@@ -0,0 +1,31 @@
+package cz.princu.adventofcode.year20.days;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Day23Test {
+
+    Day23 tested = new Day23();
+
+    @Test
+    void testPart1_simpleData() {
+
+
+        tested.setRounds(10);
+
+        String testData = "389125467";
+
+        Assertions.assertEquals("92658374", tested.part1(testData));
+
+    }
+
+    @Test
+    void testPart2_one_simpleData() {
+
+        String testData = "";
+
+        Assertions.assertEquals(0L, tested.part2(testData));
+
+    }
+
+}
