af11cf642844eed77ef6d49290f9d5f813c510f2
Jan Lopata <jan.lopata@rohlik.cz>
2021-12-12 11:10:03 +0100

    AOC 2021 - Day 07 refactor

--- src/main/java/cz/princu/adventofcode/year21/days/Day07.java
+++ src/main/java/cz/princu/adventofcode/year21/days/Day07.java
@@ -18,44 +18,43 @@ public class Day07 extends Day {
     public Object part1(String data) {
 
         String[] input = data.split(",");
-
         List<Long> inputLong = Arrays.stream(input).map(Long::parseLong).collect(Collectors.toList());
-
         var max = Arrays.stream(input).mapToInt(Integer::parseInt).max().orElse(0);
-
-        long minFuel = Long.MAX_VALUE;
-        for (int i = 0; i < max + 1; i++) {
-
-            long fuel = 0L;
-            for (Long aLong : inputLong) {
-                fuel += Math.abs(aLong - i);
-            }
-
-            log.info("{}: {}", i, fuel);
-            if (fuel < minFuel) {
-                minFuel = fuel;
-            }
-
-        }
-
-        return minFuel;
+        int[] priceTable = getSimplePriceTable(max);
+        return findMinimumFuelConsumption(inputLong, max, priceTable);
     }
 
     @Override
     public Object part2(String data) {
 
         String[] input = data.split(",");
-
         List<Long> inputLong = Arrays.stream(input).map(Long::parseLong).collect(Collectors.toList());
-
         var max = Arrays.stream(input).mapToInt(Integer::parseInt).max().orElse(0);
+        int[] priceTable = getRaisingPriceTable(max);
+        return findMinimumFuelConsumption(inputLong, max, priceTable);
+    }
 
+    private int[] getRaisingPriceTable(int max) {
         int[] priceTable = new int[max + 1];
         priceTable[0] = 0;
         for (int i = 1; i < priceTable.length; i++) {
             priceTable[i] = priceTable[i-1] + i;
         }
+        return priceTable;
+    }
 
+
+    private int[] getSimplePriceTable(int max) {
+        int[] priceTable = new int[max + 1];
+        priceTable[0] = 0;
+        for (int i = 1; i < priceTable.length; i++) {
+            priceTable[i] = i;
+        }
+        return priceTable;
+    }
+
+
+    private long findMinimumFuelConsumption(List<Long> inputLong, int max, int[] priceTable) {
         long minFuel = Long.MAX_VALUE;
         for (int i = 0; i < max + 1; i++) {
 
@@ -65,13 +64,15 @@ public class Day07 extends Day {
                 fuel += priceTable[(int) distance];
             }
 
-            log.info("{}: {}", i, fuel);
+            log.debug("{}: {}", i, fuel);
             if (fuel < minFuel) {
                 minFuel = fuel;
+            } else {
+                // fuel consumption is only raising from now
+                return minFuel;
             }
 
         }
-
         return minFuel;
     }
 
